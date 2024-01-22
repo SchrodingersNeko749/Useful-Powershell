7954c521f961c4e7ae699eb82ebcc5d4aaceb38d
SchrodingersNeko749 <44234331+SchrodingersNeko749@users.noreply.github.com>
2024-01-22 10:45:27 +0330

    Open ports and Processes
    This script searches open ports and lists the process behind them. Don't forget to run as Administrator to get more info.

---
+++ openPortsAndProcesses.ps1
@@ -0,0 +1,18 @@
+﻿Get-NetTCPConnection -State Listen | ForEach-Object {
+    $processId = $_.OwningProcess
+    $process = Get-Process -Id $processId -ErrorAction SilentlyContinue
+
+    $owner = $null
+    if ($process) {
+        $owner = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $processId").GetOwner().User
+    }
+
+    [PSCustomObject]@{
+        LocalAddress = $_.LocalAddress
+        LocalPort = $_.LocalPort
+        ProcessName = $process.Name
+        ProcessId = $process.Id
+        ProcessOwner = $owner
+        ConnectionState = $_.State
+    }
+} | Format-Table -AutoSize
